c58cad587f1a23bc15071f39e547918f9ea6a757
Avraham <avrahambenaramsouza@gmail.com>
2023-08-16 00:03:56 -0300

    moving vim setup

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
 require("avraham")
-
-vim.wo.number = true
-vim.wo.relativenumber = true
-
--- Set tab size to 4 spaces
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
--- lua/avraham/init.lua
+++ lua/avraham/init.lua
@@ -1,1 +1,9 @@
 require("avraham.remap")
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- Set tab size to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
