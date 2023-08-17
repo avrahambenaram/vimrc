29a9dfe2e6aaad40a9771671dba4bd54e25775ea
Avraham <avrahambenaramsouza@gmail.com>
2023-08-17 16:58:56 -0300

    Git integration keybindings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>gsg", vim.cmd.Git);
 vim.keymap.set("n", "<leader>gsc", ":Git commit<CR>")
+vim.keymap.set("n", "<leader>gsp", ":Git push<CR>")
+vim.keymap.set("n", "<leader>gsP", ":Git pull<CR>")
+vim.keymap.set("n", "<leader>gsb", ":Git checkout ", {noremap = false})
