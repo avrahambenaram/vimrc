7826739a32f684f92f33d8b73ce433f684aaa30d
Avraham <avrahambenaramsouza@gmail.com>
2023-08-18 00:13:57 -0300

    fugitive keybindings update

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<leader>gsg", vim.cmd.Git);
-vim.keymap.set("n", "<leader>gsc", ":Git commit<CR>")
-vim.keymap.set("n", "<leader>gsp", ":Git push<CR>")
-vim.keymap.set("n", "<leader>gsP", ":Git pull<CR>")
-vim.keymap.set("n", "<leader>gsb", ":Git checkout ", {noremap = false})
+vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+vim.keymap.set("n", "<leader>gP", ":Git pull<CR>")
+vim.keymap.set("n", "<leader>gb", ":Git checkout ", {noremap = false})
