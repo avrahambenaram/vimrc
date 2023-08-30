0d2c9687455607c0bb252909efaa2ede3b49a982
Avraham <avrahambenaramsouza@gmail.com>
2023-08-30 17:15:17 -0300

    Trouble plugin: toggle instead of open in keybindings

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,6 +1,6 @@
 local trouble = require("trouble")
-vim.keymap.set("n", "<leader>xw", function() trouble.open("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() trouble.open("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
   {silent = true, noremap = true}
 )
